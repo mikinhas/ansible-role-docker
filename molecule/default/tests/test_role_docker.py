88c66e3cbd5fbcdac7da1fac360d82566cf3e01f
Michael MACHADO <michael.machado.reino@gmail.com>
2024-02-06 22:18:33 +0100

    refactor: molecule

--- molecule/default/tests/test_role_docker.py
+++ molecule/default/tests/test_role_docker.py
@@ -7,9 +7,18 @@ def test_docker_cli_is_installed(host):
     assert package.is_installed
 
 def test_docker_buildx_is_installed(host):
+    package = host.package("containerd.io")
+    assert package.is_installed
+
+def test_docker_compose_is_installed(host):
     package = host.package("docker-buildx-plugin")
     assert package.is_installed
 
 def test_docker_compose_is_installed(host):
     package = host.package("docker-compose-plugin")
     assert package.is_installed
+
+def test_docker_service(host):
+    service = host.service("docker")
+    assert service.is_running
+    assert service.is_enabled
