506403bcf1c93f54c32386c62aebe04195915939
Michael MACHADO <michael.machado.reino@gmail.com>
2024-03-27 14:36:59 +0100

    feat: Add simple network creation

--- molecule/default/tests/test_role_docker.py
+++ molecule/default/tests/test_role_docker.py
@@ -18,7 +18,22 @@ def test_docker_compose_is_installed(host):
     package = host.package("docker-compose-plugin")
     assert package.is_installed
 
+def test_pip_is_installed(host):
+    package = host.package("python3-pip")
+    assert package.is_installed
+
 def test_docker_service(host):
     service = host.service("docker")
     assert service.is_running
     assert service.is_enabled
+
+def test_if_pip_is_installed(host):
+    pip_package = host.package("requests").is_installed
+
+def test_if_networks_foo_exists(host):
+    networks = host.run("docker network list | grep foo")
+    assert networks.rc == 0
+
+def test_if_networks_bar_exists(host):
+    networks = host.run("docker network list | grep bar")
+    assert networks.rc == 0
